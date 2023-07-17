c4f9187662fe979433b080abc1d293f018c78243
jamal <abdijumale2@gmail.com>
2023-07-18 07:46:17 +0900

    added styles to the clock

---
+++ digital Clock/scripts.js
@@ -0,0 +1,21 @@
+const hrs = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+const session = document.getElementById("session")
+
+
+function displayTime(){
+    const date = new Date()
+    hrs.innerHTML = date.getHours()
+    minutes.innerHTML = date.getMinutes()
+    seconds.innerHTML = date.getSeconds()
+    if(hrs>=12){
+        session.innerHTML = "PM"
+    }else{
+        session.innerHTML = "AM"
+    }
+    // if(hrs)
+
+
+}
+setInterval(displayTime,10)
